544ad3918127aa246224fbb27ee03ada8e116e82
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2024-12-02 18:04:00 +0530

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google';
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
@@ -32,17 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       {/* Add GTM script to <head> */}
       <Head>
         <title>{getTitle()}</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5HDDPXG');
-            `,
-          }}
-        />
+        <GoogleTagManager gtmId="GTM-5HDDPXG" />
       </Head>
       <body className={inter.className}>
         {/* Add GTM noscript to <body> */}

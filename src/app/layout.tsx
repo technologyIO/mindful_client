728aa9bbae90ae682c5eb8392d34a44259c9b706
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2024-12-02 17:39:39 +0530

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,40 +5,56 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/Footer";
 import { usePathname } from "next/navigation";
-import Head from "next/head"; // Import Head from next/head
+import Head from "next/head";
 import AdsNavbar from "@/components/AdsNavbar";
 const inter = Inter({ subsets: ["latin"] });
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const pathname = usePathname(); // Get the current route path
+export default function RootLayout({ children }: { children: React.ReactNode })  {
+  const pathname = usePathname();
 
   const noFooterPaths = ["/admin", "/assesment", "/thankyou"];
-  const noNavbarPaths = ["/ads", ];
+  const noNavbarPaths = ["/ads"];
   const zeroNav = ["/thankyou"];
   const shouldHideFooter = noFooterPaths.some((path) => pathname.startsWith(path));
   const shouldHideNavbar = noNavbarPaths.some((path) => pathname.startsWith(path));
   const hideZeroNavbar = zeroNav.some((path) => pathname.startsWith(path));
-  // Set dynamic title based on the path or other conditions
+
   const getTitle = () => {
     if (pathname === "/admin") return "Admin Panel";
     if (pathname === "/assesment") return "Assessment";
-    return "mindful TMS"; // Default title
+    return "Mindful TMS"; // Default title
   };
 
   return (
     <html lang="en">
+      {/* Add GTM script to <head> */}
       <Head>
         <title>{getTitle()}</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5HDDPXG');
+            `,
+          }}
+        />
       </Head>
       <body className={inter.className}>
-     {  shouldHideNavbar?<AdsNavbar/>:  hideZeroNavbar ? null : <Navbar />}
+        {/* Add GTM noscript to <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5HDDPXG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {shouldHideNavbar ? <AdsNavbar /> : hideZeroNavbar ? null : <Navbar />}
         <Toaster position="top-right" />
         {children}
         {!shouldHideFooter && <Footer />}

6e9a0b479c0b5e63f35bef219e56ace71c6764ab
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-01-11 17:50:09 +0530

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true,
-    webpack: (config) => {
-      config.optimization.minimize = false;
-      return config;
-    },
-    images: {
-    domains: ['mindfultms1.s3.us-east-1.amazonaws.com',
-      'ik.imagekit.io',
-      "mindfultms1.s3.amazonaws.com",
+  swcMinify: true,
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mindfultms1.s3.us-east-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mindfultms1.s3.amazonaws.com',
+      },
     ],
   },
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;

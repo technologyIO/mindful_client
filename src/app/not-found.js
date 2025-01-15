d6104383223647ba541b57a72478900597aad48c
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-01-15 16:21:15 +0530

    push

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -23,7 +23,7 @@ const NotFound = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-red-500">404</h1>
-        <p className="text-xl mb-4">Oops! The page you're looking for does not exist.</p>
+        <p className="text-xl mb-4">{`Oops! The page you're looking for does not exist.`}</p>
         <p className="text-lg">
           Redirecting to the homepage in <span className="font-bold">{countdown}</span> seconds...
         </p>

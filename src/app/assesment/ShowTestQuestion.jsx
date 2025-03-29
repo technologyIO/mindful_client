396044f9868c56eca59e08a58a24b6ea75244b75
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-03-29 16:21:52 +0530

    push

---
+++ src/app/assesment/ShowTestQuestion.jsx
@@ -0,0 +1,266 @@
+"use client";
+
+import { CircularProgress, Container } from '@mui/material';
+import axios from 'axios';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+// Define test data
+const tests = [
+  {
+    title: 'PHQ-9 Depression Test',
+    link: '/assesment-page/phq9',
+    id: 'phq9',
+    condition: "Depression",
+    paragraph: `The PHQ-9 consists of 9 questions that ask about feelings of sadness, hopelessness, and loss of interest in activities, among other symptoms.
+
+By answering these questions, you can gain insights into your depressive symptoms and determine if you might benefit from further support or professional help.`
+  },
+  {
+    title: 'PSS-10 Stress Assessment',
+    link: '/assesment-page/pss10',
+    id: 'pss10',
+    condition: "Stress",
+    paragraph: `This test measures your perception of stress and how you handle life's challenges.
+
+The PSS-10 consists of 10 questions that ask about your feelings and thoughts during the last month, including how often you felt nervous, stressed, or unable to control important things in your life.`
+  },
+  {
+    title: 'GAD-7 Anxiety Test',
+    link: '/assesment-page/gad7',
+    id: 'gad7',
+    condition: "Anxiety",
+    paragraph: `This questionnaire helps you evaluate the severity of your anxiety symptoms over the past two weeks.
+
+The GAD-7 consists of 7 questions that ask about feelings of nervousness, worry, and restlessness, among other symptoms. By answering these questions honestly, you can get a clearer picture of your anxiety levels and determine if you might benefit from further support or professional help.`
+  },
+  {
+    title: 'K10 (Kessler Psychological Distress Scale)',
+    link: '/assesment-page/k10',
+    id: 'k10',
+    paragraph: `The K10 is a simple and effective tool designed to measure your level of psychological distress over the past month. This test can help you gain insights into your mental health, especially if you're feeling anxious, depressed, or generally overwhelmed.
+
+The K10 consists of 10 questions that ask about feelings of nervousness, hopelessness, restlessness, and fatigue, among other symptoms. By answering these questions, you can get a better understanding of your current emotional state and whether you might benefit from further support or professional help.`
+  }
+];
+
+
+
+const Test = () => {
+  const searchParams = useSearchParams();
+  const testPage = searchParams.get("testPage") || false;
+  const { slugs } = useParams();
+  const router = useRouter();
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
+  const [totalScore, setTotalScore] = useState(null);
+  const [testId, setTestId] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const test = tests.find(t => t.id === slugs);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}tests/getQuestions/${test.condition}`);
+        setQuestions(response.data.questions);
+        setTestId(response.data.questions[0].testId);
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
+    };
+
+    if (test) fetchQuestions();
+  }, [test]);
+
+  const handleAnswerChange = (questionId, selectedChoiceIndex) => {
+    setAnswers({
+      ...answers,
+      [questionId]: selectedChoiceIndex,
+    });
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    window.history.pushState({ modalOpen: true }, "modal");
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    window.history.back();
+  };
+
+  const [formData, setFormData] = useState({
+    email: '',
+    phone: '',
+  });
+
+  // Function to handle form data changes
+  const handleFormDataChange = (field, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  // Function to handle email and phone submission
+  const handleEmailSubmit = async () => {
+    setLoader(true);
+    try {
+      const payload = {
+        ...formData,
+        testId: testId,
+        answers: Object.keys(answers).map((questionId) => ({
+          questionId,
+          selectedChoiceIndex: answers[questionId],
+        })),
+      };
+
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}tests/submitTest`,
+        payload
+      );
+      openModal();
+      setTotalScore(response.data.totalScore);
+    } catch (error) {
+      setLoader(false);
+      console.error('Error submitting answers:', error);
+    }
+  };
+
+  // Close modal on Escape key and handle back button
+  useEffect(() => {
+    const handleEscapeKey = (e) => {
+      if (e.key === "Escape" && isModalOpen) {
+        closeModal();
+      }
+    };
+
+    const handleBackButton = () => {
+      if (isModalOpen) {
+        setIsModalOpen(false);
+        window.history.forward();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscapeKey);
+    window.addEventListener("popstate", handleBackButton);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+      window.removeEventListener("popstate", handleBackButton);
+    };
+  }, [isModalOpen]);
+
+  const conditionIframe = {
+    "Anxiety": `https://forms.zohopublic.in/nikhilmindf1/form/SelfAssessmentTestAnxiety/formperma/Cv0aOlyHjS1THflfwBSEEEGidbLVKykg4btlwXwrp0k?source=Website%20(Self%20Assessment)&test=Anxiety&score=${totalScore}`,
+    "Depression": `https://forms.zohopublic.in/nikhilmindf1/form/SelfAssessmentTest/formperma/wQFLTUbc4MiqMDLoRnkr6NrkITyrOhdYn7ezRkLSFjk?source=Website%20(Self%20Assessment)&test=Depression&score=${totalScore}`,
+    "Stress": `https://forms.zohopublic.in/nikhilmindf1/form/SelfAssessmentTestStress/formperma/nsBzSHgvrxjEHZOuNo0IfKGsc0AxG-I1R-C2yZzQxjA?source=Website%20(Self%20Assessment)&test=Stress&score=${totalScore}`,
+  };
+
+  const TestPageconditionIframe = {
+    "Anxiety": `https://forms.zohopublic.in/nikhilmindf1/form/TestAnxiety/formperma/Es1-KWvfDKTU6WuI3HICeG4ufcf_ok47JrsnRTMu_U4?source=Website%20(test)&test=Anxiety&score=${totalScore}`,
+    "Depression": `https://forms.zohopublic.in/nikhilmindf1/form/TestDepression/formperma/GdNH4q2FAzrNJvfOdC5j7ZLZ9Rq_Hc0O2ut7JV6oeCI?source=Website%20(test)&test=Depression&score=${totalScore}`,
+    "Stress": `https://forms.zohopublic.in/nikhilmindf1/form/TestStress/formperma/lDDSNZId2A7wQzYn_T1pR4INI-ragCkW8lQsq13KOgc?source=Website%20(test)&test=Stress&score=${totalScore}`,
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <div className="flex items-center p-6">
+        <div className="mr-4 cursor-pointer" onClick={() => router.back()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M15 18l-6-6 6-6" />
+          </svg>
+        </div>
+        <h1 className="text-2xl font-bold">{test.title}</h1>
+      </div>
+
+      <div className="p-4 mx-auto space-y-8">
+        <h2 className="font-semibold mb-4 text-primary-orange text-xl text-center">
+          Please select the answer that is applicable to you
+        </h2>
+
+        {questions?.map((question, index) => (
+          <div
+            key={question._id}
+            className="bg-white rounded-lg shadow p-6 space-y-4 border border-gray-100"
+          >
+            <p className="text-lg font-semibold">
+              {index + 1}. {question.questionText}
+            </p>
+            <div className="flex flex-col md:flex-row gap-4">
+              {question.choices.map((choice, choiceIndex) => {
+                const isSelected = answers[question._id] === choiceIndex;
+                return (
+                  <button
+                    type="button"
+                    key={choiceIndex}
+                    onClick={() => handleAnswerChange(question._id, choiceIndex)}
+                    className={`w-full md:w-auto py-2 px-4 rounded border transition duration-200 whitespace-normal ${
+                      isSelected
+                        ? "bg-primary-orange border-primary-orange text-white"
+                        : "bg-gray-100 border-gray-300 text-gray-800 hover:bg-gray-200"
+                    }`}
+                  >
+                    {choice}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        ))}
+
+        <div className="flex justify-center">
+          <button
+            onClick={handleEmailSubmit}
+            className="bg-primary-orange text-white py-3 px-12 rounded-lg font-semibold hover:bg-orange-600 transition duration-200"
+          >
+            SUBMIT
+          </button>
+        </div>
+
+        {/* Modal for Email Input */}
+        {isModalOpen && (
+          <div
+            className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50"
+            onClick={closeModal}
+          >
+            <div
+              className="bg-white p-6 rounded-lg shadow-lg mx-4 w-full max-w-lg"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div style={{ width: "100%", height: "500px" }}>
+                <iframe
+                  aria-label="Self-Assessment Test"
+                  frameBorder="0"
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    border: "none",
+                  }}
+                  src={
+                    testPage
+                      ? TestPageconditionIframe[test?.condition]
+                      : conditionIframe[test?.condition]
+                  }
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default Test;
--- src/app/assesment/TakeAssessment.jsx
+++ src/app/assesment/TakeAssessment.jsx
@@ -26,7 +26,7 @@ const TakeAssessment = ({ allSection }) => {
     ]
     return (
         <>
-          <Container maxWidth="xl">
+          <Container maxWidth="lg">
           <div className="min-h-screen  p-4 sm:p-6 h-screen lg:p-8">
                 <div className=" mx-auto h-full">
                     <div className="bg-primary-div rounded-t-lg p-6 mb-6">
@@ -38,10 +38,10 @@ const TakeAssessment = ({ allSection }) => {
                     </div>
 
                     <div className="bg-white rounded-lg  p-6 mb-6">
-                        <h2 className="text-2xl font-semibold mb-4">Available Tests</h2>
+                        <h2 className="text-2xl text-center md:text-start font-semibold mb-4">Available Tests</h2>
                         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                             {testsList.map((test, index) => (
-                                <div key={index} className="bg-white rounded-lg  shadow-lg hover:shadow-lg transition-shadow duration-300">
+                                <div key={index} className="bg-white rounded-lg  border border-gray-300 transition-shadow duration-300">
                                     <div className="p-6">
                                         <h3 className="text-lg font-semibold mb-2">{test.title}</h3>
                                         <p className="text-gray-600 mb-4">{test.description}</p>
@@ -54,24 +54,7 @@ const TakeAssessment = ({ allSection }) => {
                         </div>
                     </div>
 
-                    {/* <div className="bg-primary-div rounded-lg shadow-md p-6">
-                        <h2 className="text-2xl font-semibold mb-4">Not sure which test to take?</h2>
-                        <p className="text-gray-600 mb-6">
-                            The K10 is designed to measure general psychological distress and can be an effective initial screening
-                            tool to identify whether you may need further assessment or support.
-                        </p>
-                        <div className="bg-gray-50 rounded-lg p-6">
-                            <h3 className="text-xl font-semibold mb-2">K10 (Kessler Psychological Distress Scale)</h3>
-                            <p className="text-gray-600 mb-4">
-                                This test assesses the level of distress you have experienced in the past month.
-                                It helps identify symptoms of anxiety and depression, emotional and physical fatigue,
-                                and the impact on daily functioning.
-                            </p>
-                            <button className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded transition-colors duration-300">
-                                Take the K10 Test
-                            </button>
-                        </div>
-                    </div> */}
+                  
                 </div>
             </div>
           </Container>

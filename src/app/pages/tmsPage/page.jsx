83af179b2a511cb5c22292f1c3c14f7d47a1a7be
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-04-23 10:55:24 +0530

    fix tmspage issue

--- src/app/pages/tmsPage/page.jsx
+++ src/app/pages/tmsPage/page.jsx
@@ -7,6 +7,10 @@ export async function generateMetadata() {
   return {
    title: 'rTMS Treatment in Bengaluru & Delhi | Mindful TMS Clinics',
    description:`Mindful TMS offers safe, medication-free rTMS treatment for depression, anxiety, OCD, chronic pain, trauma, and nicotine addiction in Bengaluru and Delhi.`,
+   robots: {
+     index: false,
+     follow: false,
+   },
  };
 
 }

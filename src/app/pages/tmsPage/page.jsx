fdfeff23915e70c05430533c236f4d74ac469a1d
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-04-23 11:48:16 +0530

    fix middle ware

--- src/app/pages/tmsPage/page.jsx
+++ src/app/pages/tmsPage/page.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import TmsMainPage from '@/app/component/TmsMainPage'
 
 export async function generateMetadata() {
-
-
   return {
    title: 'rTMS Treatment in Bengaluru & Delhi | Mindful TMS Clinics',
    description:`Mindful TMS offers safe, medication-free rTMS treatment for depression, anxiety, OCD, chronic pain, trauma, and nicotine addiction in Bengaluru and Delhi.`,
@@ -12,7 +10,6 @@ export async function generateMetadata() {
      follow: false,
    },
  };
-
 }
 
 const Page = () => {

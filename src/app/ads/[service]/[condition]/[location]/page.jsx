33f70242e4e123967755d6718c321d627ba3e45d
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-06-07 15:31:51 +0530

    push

--- src/app/ads/[service]/[condition]/[location]/page.jsx
+++ src/app/ads/[service]/[condition]/[location]/page.jsx
@@ -11,7 +11,7 @@ export async function generateMetadata({params}) {
    description:`Are you looking for an experienced ${params.service}?. We help people with anxiety, depression, OCD, grief, trauma, and more in a safe space.
 
 Whether you're facing stress, seeking personal growth, or need someone to talk to, our compassionate Psychologists are here for you.`,
- robots: "noindex, nofollow",
+ robots: "index, follow",
  };
 
 }
